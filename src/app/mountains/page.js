7718944536afbaf00266ce6b23f5676db7598c14
Dana <dana@yellowpencil.com>
2025-01-31 13:54:11 -0700

    Creating routes

---
+++ src/app/mountains/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Mountains</h1>
+    </main>
+  )
+}
---
+++ src/app/trees/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Tree information</h1>
+    </main>
+  )
+}
